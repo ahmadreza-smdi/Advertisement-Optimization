c70051c687c38215f06c48898b07bec4a2666f0a
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2020-02-01 16:57:08 +0330

    create data base; Website,Adv

--- Advertisement/ads/admin.py
+++ Advertisement/ads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Adv,Website
 # Register your models here.
+
+
+admin.site.register(Adv)
+admin.site.register(Website)
--- Advertisement/ads/models.py
+++ Advertisement/ads/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Website(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Adv(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    context = models.CharField(max_length=2000)
+    Related_website_url = models.CharField(max_length=50)
+    def __str__(self):
+        return self.Related_website_url
